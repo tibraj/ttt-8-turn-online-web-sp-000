a32e1a095e39b03a62f0b90353a67d2defe63010
Taulant Ibraj <tibraj94@gmail.com>
2020-03-21 17:34:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ def move (array, index, value = "X")
 end
 
 def position_taken?(board, index)
-  if board[index] != " "
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     return false
   else
     return true
