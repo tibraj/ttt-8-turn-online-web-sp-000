16bb14a2df9083335b2c0421fe20838e13542d1f
Taulant Ibraj <tibraj94@gmail.com>
2020-03-21 16:12:17 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(userinput)
+  userinput.to_i - 1
+end
+def move (array, index1, value = "X")
+  array[index1] = value
+end
+
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
