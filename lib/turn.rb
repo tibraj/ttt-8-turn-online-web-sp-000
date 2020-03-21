1f2a650593ae8ae30aeaffb07918c26753fb08ff
Taulant Ibraj <tibraj94@gmail.com>
2020-03-21 16:56:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,5 +32,5 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_move = gets.to_i
-  valid_move?(board, input_to_index(user_move))
+  processing = valid_move?(board, input_to_index(user_move))
 end
